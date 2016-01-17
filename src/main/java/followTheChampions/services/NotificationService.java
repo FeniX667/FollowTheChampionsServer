f233a89b6aaaebbde01a7b4d5c5a534f5a7ab1c1
Damian Malarczyk <dmalarczyk@gmail.com>
2016-01-17 12:15:55 +0100

    Final build.

--- src/main/java/followTheChampions/services/NotificationService.java
+++ src/main/java/followTheChampions/services/NotificationService.java
@@ -1,6 +1,7 @@
 package followTheChampions.services;
 
 import com.google.android.gcm.server.Message;
+import followTheChampions.dao.MatchEventRepository;
 import followTheChampions.dao.MatchRepository;
 import followTheChampions.dao.RegisteredDeviceRepository;
 import followTheChampions.dto.Notification;
@@ -24,6 +25,12 @@ public class NotificationService {
     private final static Logger logger = LoggerFactory
             .getLogger(NotificationService.class);
 
+    private static String CARD_SOUND = "card_sound.mp3";
+    private static String GOAL_SOUND = "goal_sound.mp3";
+    private static String HALF_START_SOUND = "half_start_sound.mp3";
+    private static String FIRST_HALF_END_SOUND = "first_half_end_sound.mp3";
+    private static String MATCH_END_SOUND = "match_end_sound.mp3";
+
     @Autowired
     IosNotificationPusher iosNotificationPusher;
 
@@ -35,6 +42,9 @@ public class NotificationService {
 
     @Autowired
     MatchRepository matchRepository;
+
+    @Autowired
+    MatchEventRepository matchEventRepository;
 
     //@Async
     public void sendAsNotification(MatchEvent event){
@@ -94,6 +104,21 @@ public class NotificationService {
         notification.getPayload().put("player", event.getPlayerName());
         notification.getPayload().put("result", event.getResult());
 
+        switch(event.getType()){
+            case "goal":
+                notification.getPayload().put("sound", GOAL_SOUND);
+                break;
+
+            case "yellowcard":
+                notification.getPayload().put("sound", CARD_SOUND);
+                break;
+
+            case "redcard":
+                notification.getPayload().put("sound", CARD_SOUND);
+                break;
+        }
+
+
         return notification;
     }
 
@@ -107,18 +132,22 @@ public class NotificationService {
         switch(match.getStatus()){
             case "1" :
                 aps.put("alert", match.getMatchName() + " started! " );
+                aps.put("sound", this.HALF_START_SOUND);
                 break;
 
             case "HT" :
                 aps.put("alert", match.getMatchName() + " first half finished. " );
+                aps.put("sound", this.FIRST_HALF_END_SOUND);
                 break;
 
             case "46" :
                 aps.put("alert", match.getMatchName() + " second half started. " );
+                aps.put("sound", this.HALF_START_SOUND);
                 break;
 
             case "FT" :
                 aps.put("alert", match.getMatchName() + " is finished. " );
+                aps.put("sound", this.MATCH_END_SOUND);
                 break;
         }
 
@@ -147,23 +176,27 @@ public class NotificationService {
     private Message createAndroidMessage(Match match) {
         Message.Builder messageBuilder =  new Message.Builder();
         String message = new String();
-
+        String sound = new String();
         String status = match.getStatus();
         switch(match.getStatus()){
             case "1" :
                 message = match.getMatchName() + " started! ";
+                sound = this.HALF_START_SOUND;
                 break;
 
             case "HT" :
                 message = match.getMatchName() + " first half finished. " ;
+                sound = this.FIRST_HALF_END_SOUND;
                 break;
 
             case "46" :
                 message = match.getMatchName() + " second half started. " ;
+                sound = this.HALF_START_SOUND;
                 break;
 
             case "FT" :
                 message = match.getMatchName() + " is finished. " ;
+                sound = this.MATCH_END_SOUND;
                 break;
         }
 
@@ -171,6 +204,7 @@ public class NotificationService {
                 .addData("text", message)
                 .addData("match", match.getId().toString())
                 .addData("status", status)
+                .addData("sound", sound)
                 .delayWhileIdle(true)
                 .collapseKey(status)
                 .timeToLive(30 * 60);
@@ -197,16 +231,77 @@ public class NotificationService {
     public void fakeStatusNotification() {
         logger.info("Preparing status notification");
         Match match = matchRepository.getById(1L);
+
         match.setStatus("1");
         sendAsNotification(match);
+
+        try {
+            Thread.sleep(10000);                 //1000 milliseconds is one second.
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+
+        MatchEvent fakeMatchEvent = new MatchEvent();
+        fakeMatchEvent.setMinute("15");
+        fakeMatchEvent.setWhichTeam("local");
+        fakeMatchEvent.setMatch(match);
+        fakeMatchEvent.setId(1L);
+        fakeMatchEvent.setPlayerName("Malarinhio");
+        fakeMatchEvent.setType("goal");
+        fakeMatchEvent.setResult("[1 - 0");
+
+        matchEventRepository.save(fakeMatchEvent);
+        match.getMatchEventList().add(fakeMatchEvent);
+
+        sendAsNotification(fakeMatchEvent);
+
+        try {
+            Thread.sleep(10000);                 //1000 milliseconds is one second.
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+
+        MatchEvent fakeMatchEvent2 = new MatchEvent();
+        fakeMatchEvent2.setMinute("23");
+        fakeMatchEvent2.setWhichTeam("local");
+        fakeMatchEvent2.setMatch(match);
+        fakeMatchEvent2.setId(2L);
+        fakeMatchEvent2.setPlayerName("Malarinhio");
+        fakeMatchEvent2.setType("yellowcard");
+
+        matchEventRepository.save(fakeMatchEvent);
+        match.getMatchEventList().add(fakeMatchEvent);
+
+        sendAsNotification(fakeMatchEvent2);
+
+        try {
+            Thread.sleep(10000);                 //1000 milliseconds is one second.
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
 
         match.setStatus("HT");
         sendAsNotification(match);
 
+        try {
+            Thread.sleep(10000);                 //1000 milliseconds is one second.
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+
         match.setStatus("46");
         sendAsNotification(match);
 
+        try {
+            Thread.sleep(10000);                 //1000 milliseconds is one second.
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+
         match.setStatus("FT");
         sendAsNotification(match);
+
+        matchEventRepository.delete(1L);
+        matchEventRepository.delete(2L);
     }
 }
